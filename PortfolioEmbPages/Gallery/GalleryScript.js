88aa423eb0ddc116505c17451ef45e6bb504803f
DarttGoblin <112183975+DarttGoblin@users.noreply.github.com>
2024-08-09 19:01:04 +0100

    Update GalleryScript.js

--- PortfolioEmbPages/Gallery/GalleryScript.js
+++ PortfolioEmbPages/Gallery/GalleryScript.js
@@ -5,40 +5,54 @@ const projectCaptionDiv = document.getElementsByClassName('projectCaptionDiv');
 const iBulkCaption = document.getElementById('iBulkCaption');
 const academicnexusCaption = document.getElementById('academicnexusCaption');
 const logSpectrumCaption = document.getElementById('logSpectrumCaption');
+const whisperCaption = document.getElementById('whisperCaption');
 
-const projectLinks = ["https://darttgoblin.github.io/iBulk/iBulk.html"];
-const latestProjectLinks = ["https://darttgoblin.github.io/LogSpectrum/Home.html"];
+const projectLinks = [
+    "https://darttgoblin.github.io/iBulk/iBulk.html",
+    "not_available",
+    "https://darttgoblin.github.io/LogSpectrum/Home.html",
+];
+const latestProjectLinks = ["https://darttgoblin.github.io/Whisper/Login.html"];
 
 const iBulkCaptions = ["GalleryMedia/iBulkCaptions/home.png", "GalleryMedia/iBulkCaptions/programs.png", "GalleryMedia/iBulkCaptions/workout.png", "GalleryMedia/iBulkCaptions/nutrition.png", "GalleryMedia/iBulkCaptions/services.png", "GalleryMedia/iBulkCaptions/about.png"];
 const academicnexusCaptions = ["GalleryMedia/AcademicNexusCaptions/home.png", "GalleryMedia/AcademicNexusCaptions/login.png","GalleryMedia/AcademicNexusCaptions/crud.png","GalleryMedia/AcademicNexusCaptions/addstudent.png"];
 const logSpectrumCaptions = ["GalleryMedia/LogSpectrumCaptions/dashboard.png", "GalleryMedia/LogSpectrumCaptions/log.png", "GalleryMedia/LogSpectrumCaptions/report.png", "GalleryMedia/LogSpectrumCaptions/help.png"];
+const whisperCaptions = ["GalleryMedia/WhisperCaptions/login.png", "GalleryMedia/WhisperCaptions/home.png", "GalleryMedia/WhisperCaptions/group.png"];
 
 var dotsIndex = 0;
 var ibulk_index = 0;
 var aca_nex_index = 0;
 var ls_index = 0;
+var whisper_index = 0;
 
 ClickOnLink();
 setInterval(SwitchDots, 500);
-AnimateCaptions(ibulk_index, aca_nex_index, ls_index);
+AnimateCaptions(ibulk_index, aca_nex_index, ls_index, whisper_index);
+
 var interval = setInterval(() => {
     ibulk_index++;
     aca_nex_index++;
     ls_index++;
+    whisper_index++;
     if (ibulk_index >= iBulkCaptions.length) {ibulk_index = 0;}
     if (aca_nex_index >= academicnexusCaptions.length) {aca_nex_index = 0;}
     if (ls_index >= logSpectrumCaptions.length) {ls_index = 0;}
-    AnimateCaptions(ibulk_index, aca_nex_index, ls_index);
+    if (whisper_index >= whisperCaptions.length) {whisper_index = 0;}
+    AnimateCaptions(ibulk_index, aca_nex_index, ls_index, whisper_index);
 }, 5000);
 
 function ClickOnLink() {
-    for (let i = 0; i < projectLinks.length; i++) {visitProject[i].onclick  = function() {
-        if (projectLinks[i] == "notavailable") {alert('Website is not currectly available! check later.');}
-        window.open(projectLinks[i], '_blank');}
+    for (let i = 0; i < projectLinks.length; i++) {
+        visitProject[i].onclick  = function() {
+            if (projectLinks[i] == "notavailable") {alert('Website is not currently available! check later.');}
+            window.open(projectLinks[i], '_blank');
+        }
     }
     for (let i = 0; i < latestProjectLinks.length; i++) {
-        if (visitLatestProject[i] == "notavailable") {alert('Website is not currectly available! check later.');}
-        visitLatestProject[i].onclick  = function() {window.open(latestProjectLinks[i], '_blank');}
+        visitLatestProject[i].onclick = function() {
+            if (latestProjectLinks[i] == "notavailable") {alert('Website is not currently available! check later.');}
+            window.open(latestProjectLinks[i], '_blank');
+        }
     }
 }
 
@@ -48,20 +62,15 @@ function StyleDots(Thedot, opacityScale, visibilityScale) {
 }
 
 function SwitchDots() {
-    if (dotsIndex == 3) {
-        for (var i = 0; i < dots.length; i++) {StyleDots(dots[i], 0, "hidden");}
-        dotsIndex = 0;
-    }
-    else {
-        StyleDots(dots[dotsIndex], 1, "visible");
-        return dotsIndex++;
-    }
+    if (dotsIndex == 3) {for (var i = 0; i < dots.length; i++) {StyleDots(dots[i], 0, "hidden");} dotsIndex = 0;}
+    else {StyleDots(dots[dotsIndex], 1, "visible"); return dotsIndex++;}
 }
 
-function AnimateCaptions(ibulk_index, aca_nex_index, ls_index) {
+function AnimateCaptions(ibulk_index, aca_nex_index, ls_index, whisper_index) {
     if (iBulkCaption) {iBulkCaption.src = iBulkCaptions[ibulk_index];}
     if (academicnexusCaption)  {academicnexusCaption.src = academicnexusCaptions[aca_nex_index];}
     if (logSpectrumCaption) {logSpectrumCaption.src = logSpectrumCaptions[ls_index];}
+    if (whisperCaption) {whisperCaption.src = whisperCaptions[whisper_index];}
 
     for (var i = 0; i < projectCaptionDiv.length; i++) {
         (function(i) {
