b530b0060791b4600070dfd0ff6ccd1034241a35
DarttGoblin <112183975+DarttGoblin@users.noreply.github.com>
2025-01-26 14:44:52 +0100

    Update Tool.js

--- src/Components/Tool/Tool.js
+++ src/Components/Tool/Tool.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import './Tool.css'
 
-const Tool = ({image, title, usecase, color}) => {
+const Tool = ({image, title, usecase, color, long_title}) => {
   return (
     <div className='tool'>
         <div className='tool-image-container' style={{backgroundColor: color}}>
             <img className='tool-image' src={image} alt={title}></img>
         </div>
         <div className='tool-text-content'>
-            <span className='tool-title'>{title}</span>
+            <span className={long_title ? 'tool-title long-title' : 'tool-title'}>{title}</span>
             <span className='tool-usecase'>{usecase}</span>
         </div>
     </div>
