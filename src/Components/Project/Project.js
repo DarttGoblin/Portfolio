d6a8e7dcc77d12d44ab5628bac12da606b61b465
DarttGoblin <112183975+DarttGoblin@users.noreply.github.com>
2025-02-09 18:13:22 +0100

    Update Project.js

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import './Project.css'
 
-const Project = ({onClick, title, date, description, tools, available}) => {
+const Project = ({onClick, title, skill, date, description, tools, available}) => {
     return (
         <div onClick={onClick} className={available ? 'project-container' : 'project-container not-available'}>
-            <div className='project-title-date-container'>
-                <span className='project-title'>{title}</span>
-                <span className='project-date'>{date}</span>
+            <div className='project-title-skill-date-container'>
+                <div className='project-title-date-container'>
+                    <span className='project-title'>{title}</span>
+                    <span className='project-date'>{date}</span>
+                </div>
+                <span className='project-skill'>{skill}</span>
             </div>
             <span className='project-description'>{description}</span>
             <div className='tools-container'>
